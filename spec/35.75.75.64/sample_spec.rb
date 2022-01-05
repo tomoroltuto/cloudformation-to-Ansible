7c36c6450c4275a89805b1e02d5d2d63aecb2bd2
tomoroltuto <to1mo2.to2mo3@gmail.com>
2022-01-05 15:12:22 +0000

    CircleCIでserverspecを実行できるように

--- spec/35.75.75.64/sample_spec.rb
+++ spec/35.75.75.64/sample_spec.rb
@@ -10,18 +10,33 @@ describe package('wget') do
   it { should be_installed }
 end  
 
-describe package('rails -v') do
-  it { should be_installed.by('gem').with_version('6.0.3') }
+%w{ rails bundler }.each do |pkg|
+  describe package(pkg) do
+    it { should be_installed.by('gem') }
+  end
+end
+
+describe command('ruby -v') do
+  its(:stdout) { should match /ruby 2.6.3/ }
+end
+
+describe command('rails -v') do
+  its(:stderr) { should match /Rails 6.0.3/ }
+end
+
+describe service('nginx') do
+  it { should be_installed }
 end
 
 describe file('/usr/local/rbenv') do
   it { should be_file }
 end
 
-describe package('nginx') do
-  it { should be_installed }
-end
-
 describe file('/etc/nginx/nginx.conf') do
   it { should be_file }
+end
+
+describe service('nginx') do
+  it { should be_enabled }
+  it { should be_running }
 end
