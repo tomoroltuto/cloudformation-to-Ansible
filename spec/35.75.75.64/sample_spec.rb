a683eb4e2e4d58f3e0df387320f69285c87c3727
tomoroltuto <to1mo2.to2mo3@gmail.com>
2022-01-05 07:34:17 +0000

    CircleCIでserverspecを実行できるように

--- spec/35.75.75.64/sample_spec.rb
+++ spec/35.75.75.64/sample_spec.rb
@@ -1,7 +1,21 @@
 require 'spec_helper'
 
 #パッケージがインストールされているか確認する
-describe package('git') do
+describe package('httpd') do
   it { should be_installed }
 end   
 
+#パッケージがインストールされているか確認する
+describe package('node') do
+  it { should be_installed }
+end   
+
+#パッケージがインストールされているか確認する
+describe package('wget') do
+  it { should be_installed }
+end   
+
+#パッケージがインストールされているか確認する
+describe package('yarn') do
+  it { should be_installed }
+end   
