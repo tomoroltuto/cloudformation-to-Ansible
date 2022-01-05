d6563546d184e0b0f1b9ef9390a1a68be55cde58
tomoroltuto <to1mo2.to2mo3@gmail.com>
2022-01-05 07:57:51 +0000

    CircleCIでserverspecを実行できるように

--- spec/35.75.75.64/sample_spec.rb
+++ spec/35.75.75.64/sample_spec.rb
@@ -6,16 +6,15 @@ describe package('git') do
 end   
 
 #パッケージがインストールされているか確認する
-describe package('nodejs') do
-  it { should be_installed }
-end   
-
-#パッケージがインストールされているか確認する
 describe package('wget') do
   it { should be_installed }
 end   
 
-#パッケージがインストールされているか確認する
-describe package('ruby -v2.6.3') do
-  it { should be_installed }
-end   
+describe command('ruby -v') do
+  its(:stdout) { should match /ruby 2\.6\.3/ }
+end
+
+
+describe command('rails -v') do
+  its(:stdout) { should match /Rails 6\.0\.3/ }
+end
