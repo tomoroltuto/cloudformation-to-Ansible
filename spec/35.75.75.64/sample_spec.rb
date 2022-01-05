009282ef926c227a55930d7723c2e04d46e655f7
tomoroltuto <to1mo2.to2mo3@gmail.com>
2022-01-05 11:33:51 +0000

    CircleCIでserverspecを実行できるように

--- spec/35.75.75.64/sample_spec.rb
+++ spec/35.75.75.64/sample_spec.rb
@@ -1,5 +1,15 @@
 require 'spec_helper'
 
+describe host('target.example.jp') do
+  it { should be_reachable.with( :port => 22 ) }
+  it { should be_reachable.with( :port => 80 ) } 
+end
+  
+describe host('example.jp') do
+  its(:ipv4_address) { should eq '35.75.75.64' }
+end
+
+
 #パッケージがインストールされているか確認する
 describe package('git') do
   it { should be_installed }
