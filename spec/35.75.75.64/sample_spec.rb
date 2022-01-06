eaf64bedfeac00aaf00626ca1feac1ebef0b7bd2
tomoroltuto <to1mo2.to2mo3@gmail.com>
2022-01-06 06:56:20 +0000

    CircleCIでserverspecを実行できるように

--- spec/35.75.75.64/sample_spec.rb
+++ spec/35.75.75.64/sample_spec.rb
@@ -1,32 +1,45 @@
 require 'spec_helper'
 
+describe group('ec2-user') do
+  it { should exist }
+end
+
+def os_platform_amazon?
+  Specinfra.backend.run_command('uname -r').stdout.include?("amzn2")
+end
+
+describe port("22") do
+  it { should be_listening }
+end
+
+describe port("80") do
+  it { should be_listening }
+end
+
 #パッケージがインストールされているか確認する
 describe package('git') do
   it { should be_installed }
 end   
 
 #パッケージがインストールされているか確認する
-describe package('wget') do
-  it { should be_installed }
-end  
-
-%w{ "rails bundler" }.each do |pkg|
+%w{gcc gcc++ openssl-devel libyaml-devel readline-devel zlib-devel mysql }.each do |pkg|
   describe package(pkg) do
-    it { should be_installed.by('gem') }
+    it { should be_installed }
   end
 end
 
+describe package('bundler') do
+  it { should be_installed.by('gem').with_version('2.3.4') }
+end
+
 describe command('ruby -v') do
-  its(:stdout) { should match /"ruby 2.6.3"/ }
+  its(:stdout) { should match /ruby 2\.6\.3/ }
 end
 
 describe command('rails -v') do
-  its(:stdout) { should match /"Rails 6.0.3"/  }
+  its(:stdout) { should match /"Rails 6\.0\.3/  }
 end
 
-describe file('/usr/local/rbenv') do
-  it { should be_file }
-end
 
 describe file('/etc/nginx/nginx.conf') do
   it { should be_file }
@@ -34,5 +47,4 @@ end
 
 describe service('nginx') do
   it { should be_enabled }
-  it { should be_running }
 end
