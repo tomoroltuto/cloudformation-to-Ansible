dda59e1b3710c31c929ce97b49df24782e08c78a
tomoroltuto <to1mo2.to2mo3@gmail.com>
2022-01-06 05:11:53 +0000

    CircleCIでserverspecを実行できるように

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -20,7 +20,7 @@ options = Net::SSH::Config.for(host)
 
 options[:user] = "ec2-user"
 options[:keys] = ENV['KEY'];
-options[:passphrase] = ENV['PASSPHRASE'];
+
 
 set :host,        options[:host_name] || host
 set :ssh_options, options
