673c011ed12a1021df43ecacd338a78e15284354
tomoroltuto <to1mo2.to2mo3@gmail.com>
2022-01-08 14:18:14 +0000

    CircleCIでserverspecを実行できるように

--- roles/db/db.sh
+++
@@ -1,7 +0,0 @@
- export ANSIBLE_RAILS_DATABASE_HOST=$(aws ssm get-parameters --region ap-northeast-1 --name ANSIBLE_RAILS_DATABASE_HOST --query "Parameters[0].Value" --output text)
-
- export ANSIBLE_RAILS_DATABASE_PASSWORD=$(aws ssm get-parameters --region ap-northeast-1 --name ANSIBLE_RAILS_DATABASE_PASSWORD --query "Parameters[0].Value" --output text)
-
- export ANSIBLE_RAILS_USERNAME=$(aws ssm get-parameters --region ap-northeast-1 --name  ANSIBLE_RAILS_USERNAME --query "Parameters[0].Value" --output text)
- 
- printenv | grep RAILS
